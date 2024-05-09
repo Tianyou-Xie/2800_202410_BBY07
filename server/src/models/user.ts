19a47dcaf5e686b704a76b224898d6c5580eefec
Zyrakia <ole.lammers@pm.me>
2024-05-09 16:35:57 -0700

    added create timestamp to user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -10,18 +10,22 @@ interface IUser {
 	avatarUrl?: string;
 	followerCount: number;
 	followingCount: number;
+	createdAt: Date;
 }
 
-const schema = new Schema<IUser>({
-	email: { type: 'string', required: true },
-	password: { type: 'string', required: true },
-	userName: { type: 'string', required: true },
-	bio: { type: 'string' },
-	locationId: { type: 'ObjectId', ref: 'Location', required: true },
-	birthDate: { type: 'date' },
-	avatarUrl: { type: 'string' },
-	followerCount: { type: 'number', required: true, default: 0 },
-	followingCount: { type: 'number', required: true, default: 0 },
-});
+const schema = new Schema<IUser>(
+	{
+		email: { type: 'string', required: true },
+		password: { type: 'string', required: true },
+		userName: { type: 'string', required: true },
+		bio: { type: 'string' },
+		locationId: { type: 'ObjectId', ref: 'Location', required: true },
+		birthDate: { type: 'date' },
+		avatarUrl: { type: 'string' },
+		followerCount: { type: 'number', required: true, default: 0 },
+		followingCount: { type: 'number', required: true, default: 0 },
+	},
+	{ timestamps: { createdAt: true, updatedAt: false } },
+);
 
 export const UserModel = model('User', schema);
