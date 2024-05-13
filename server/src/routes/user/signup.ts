f9dbdf32ef1c492645e9b233e2da1d7a12b5f802
Zyrakia <ole.lammers@pm.me>
2024-05-13 14:23:40 -0700

    added custom messages to signup body validation

--- server/src/routes/user/signup.ts
+++ server/src/routes/user/signup.ts
@@ -2,8 +2,9 @@ import Joi from 'joi';
 import { Handler } from 'express';
 import { UserModel } from '../../models/user';
 import { createHash } from '../../utils/bcrypt';
+import { PlanetModel } from '../../models/planet';
 
-interface Body {
+interface PostBody {
 	email: string;
 	userName: string;
 	password: string;
@@ -15,14 +16,37 @@ interface Body {
 }
 
 export const post: Handler = async (req, res) => {
-	const bodySchema = Joi.object<Body>({
-		email: Joi.string().trim().email().required(),
-		userName: Joi.string().trim().required(),
-		password: Joi.string().required(),
+	const bodySchema = Joi.object<PostBody>({
+		email: Joi.string().trim().email().required().messages({
+			'string.base': 'The given email must be a string.',
+			'string.email': 'The given email is invalid.',
+			'any.required': 'Email is required.',
+		}),
+		userName: Joi.string().trim().required().messages({
+			'string.base': 'The given username must be a string.',
+			'any.required': 'Username is required.',
+		}),
+		password: Joi.string().required().messages({
+			'string.base': 'The given password must be a string.',
+			'any.required': 'Password is required.',
+		}),
 		location: Joi.object({
-			planetId: Joi.string().trim(),
-			latitude: Joi.number().min(0).max(90).required(),
-			longitude: Joi.number().min(0).max(90).required(),
+			planetId: Joi.string().trim().required().messages({
+				'string.base': 'The given planet ID must be a string.',
+				'any.required': 'Location planet ID is required.',
+			}),
+			latitude: Joi.number().min(0).max(90).required().messages({
+				'number.base': 'The given latitude must be a number.',
+				'number.min': 'The given latitude must be above 0.',
+				'number.max': 'The given latitude must be below 90.',
+				'any.required': 'Location latitude is required.',
+			}),
+			longitude: Joi.number().min(0).max(90).required().messages({
+				'number.base': 'The given longitude must be a number.',
+				'number.min': 'The given longitude must be above 0.',
+				'number.max': 'The given longitude must be below 90.',
+				'any.required': 'Location longitude is required.',
+			}),
 		}),
 	});
 
@@ -31,8 +55,11 @@ export const post: Handler = async (req, res) => {
 
 	const { value: body } = bodyValidationResult;
 
-	const existingEmail = await UserModel.findOne({ email: body.email });
-	if (existingEmail) return res.status(422).json({ error: 'Email is already registered.' });
+	const existingEmail = await UserModel.findOne({ email: body.email }).lean().exec();
+	if (existingEmail) return res.status(422).json({ error: `Email is already registered.` });
+
+	const planet = await PlanetModel.findById(body.location.planetId).lean().exec();
+	if (!planet) return res.status(400).json({ error: 'The given location does not exist.' });
 
 	const user = new UserModel({
 		email: body.email,
