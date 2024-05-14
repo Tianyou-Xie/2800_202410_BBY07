040e9cf3ed6ef7c999d2e0f3837506f8faae2d18
Zyrakia <ole.lammers@pm.me>
2024-05-14 10:08:00 -0700

    fixed signup error when casting planet id

--- server/src/routes/user/signup.ts
+++ server/src/routes/user/signup.ts
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import mongoose from 'mongoose';
 import { Handler } from 'express';
 import { UserModel } from '../../models/user';
 import { createHash } from '../../utils/bcrypt';
@@ -35,19 +36,21 @@ export const post: Handler = async (req, res) => {
 				'string.base': 'The given planet ID must be a string.',
 				'any.required': 'Location planet ID is required.',
 			}),
-			latitude: Joi.number().min(0).max(90).required().messages({
+			latitude: Joi.number().min(-90).max(90).required().messages({
 				'number.base': 'The given latitude must be a number.',
-				'number.min': 'The given latitude must be above 0.',
+				'number.min': 'The given latitude must be above -90.',
 				'number.max': 'The given latitude must be below 90.',
 				'any.required': 'Location latitude is required.',
 			}),
-			longitude: Joi.number().min(0).max(90).required().messages({
+			longitude: Joi.number().min(-180).max(180).required().messages({
 				'number.base': 'The given longitude must be a number.',
-				'number.min': 'The given longitude must be above 0.',
-				'number.max': 'The given longitude must be below 90.',
+				'number.min': 'The given longitude must be above -180.',
+				'number.max': 'The given longitude must be below 180.',
 				'any.required': 'Location longitude is required.',
 			}),
-		}),
+		})
+			.required()
+			.messages({ 'any.required': 'Location is required' }),
 	});
 
 	const bodyValidationResult = bodySchema.validate(req.body);
@@ -57,6 +60,9 @@ export const post: Handler = async (req, res) => {
 
 	const existingEmail = await UserModel.findOne({ email: body.email }).lean().exec();
 	if (existingEmail) return res.status(422).json({ error: `Email is already registered.` });
+
+	const isPlanetId = mongoose.isValidObjectId(body.location.planetId);
+	if (!isPlanetId) return res.status(400).json({ error: 'The given planet ID is invalid.' });
 
 	const planet = await PlanetModel.findById(body.location.planetId).lean().exec();
 	if (!planet) return res.status(400).json({ error: 'The given location does not exist.' });
