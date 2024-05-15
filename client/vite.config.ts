9d5ab95224d659e0683b89bf8336dadb03eb4b12
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-14 17:50:29 -0700

    changed the server port to 8000

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	plugins: [react()],
 	css: { modules: { localsConvention: 'camelCaseOnly' } },
+    server: {
+        port: 8000
+    }
 });
