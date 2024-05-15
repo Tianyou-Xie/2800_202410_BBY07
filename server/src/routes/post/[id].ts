044a7a9fb91f8e5c9a7aa0a378a4547167cb0f84
Zyrakia <ole.lammers@pm.me>
2024-05-15 12:35:23 -0700

    added post get and delete routes

---
+++ server/src/routes/post/[id].ts
@@ -0,0 +1,32 @@
+import { Handler } from 'express';
+import mongoose from 'mongoose';
+import { Resolve } from '../../utils/express';
+import { PostModel } from '../../models/post';
+import { requireLogin } from '../../middlewares/require-login';
+
+export const get: Handler = async (req, res) => {
+	const id = req.params.id;
+	if (!mongoose.isValidObjectId(id)) return Resolve(res).badRequest('Invalid post ID provided.');
+
+	const post = await PostModel.findById(id);
+	if (!post) return Resolve(res).notFound('Invalid post ID provided.');
+
+	return Resolve(res).okWith(post);
+};
+
+export const del: Handler[] = [
+	requireLogin,
+	async (req, res) => {
+		const id = req.params.id;
+		if (!mongoose.isValidObjectId(id)) return Resolve(res).badRequest('Invalid post ID provided.');
+
+		const post = await PostModel.findById(id);
+		if (!post) return Resolve(res).notFound('Invalid post ID provided.');
+
+		const loggedInId = req.session.user!.id;
+		if (!post.authorId.equals(loggedInId)) return Resolve(res).forbidden('You cannot delete this post.');
+
+		await post.deleteOne();
+		Resolve(res).okWith(post, 'Post has been deleted.');
+	},
+];
