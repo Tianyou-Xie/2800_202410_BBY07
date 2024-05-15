4f2e50474d55276d2132f994fcb31f4c9c479ac8
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-15 08:27:23 -0700

    changed index route

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,21 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './index.css';
 
 import { Route, Switch } from 'wouter';
-import IndexPage from './pages';
-import GoodbyePage from './pages/goodbye';
-import About from './pages/about';
-import Signup from './pages/signup';
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import './index.css';
+import GoodbyePage from './pages/goodbye/goodbye';
+import About from './pages/about/about';
+import Signup from './pages/signup/signup';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Switch>			
-			<Route path='/' component={IndexPage} />
+		<Switch>
+			<Route path='/' component={Signup} />
 			<Route path='/goodbye' component={GoodbyePage} />
 			<Route path='/about' component={About} />
 			<Route path='/signup' component={Signup} />
