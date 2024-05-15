1a080b35ec3060d64961f57bda38017b7001befb
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-05-15 02:01:18 -0700

    fixed the basic fading animation on the menu icon

--- client/src/components/Hotbar/Hotbar.tsx
+++ client/src/components/Hotbar/Hotbar.tsx
@@ -9,7 +9,6 @@ import { IoCloseCircleOutline } from "react-icons/io5";
 const Hotbar = () => {
 
 	function toggleMenu() {
-		console.log('here1');
 		let menuIcon = document.getElementById("menu-icon");
 		let closeIcon = document.getElementById("close-icon");
 
@@ -18,31 +17,6 @@ const Hotbar = () => {
 			menuIcon.classList.toggle('active');
 			closeIcon.classList.toggle('non-active');
 			closeIcon.classList.toggle('active');
-			if (closeIcon.classList.contains('active')) {
-				closeIcon.style.zIndex= '5';
-				menuIcon.style.zIndex= '4';
-			} else {
-				closeIcon.style.zIndex= '4';
-				menuIcon.style.zIndex= '5';
-			}
-		}		
-	}
-
-	function toggleClose() {
-		console.log('here2');
-		let menuIcon = document.getElementById("menu-icon");
-		let closeIcon = document.getElementById("close-icon");
-
-		if (menuIcon !== null && closeIcon !== null) {
-			menuIcon.classList.toggle('non-active');
-			menuIcon.classList.toggle('active');
-			closeIcon.classList.toggle('non-active');
-			closeIcon.classList.toggle('active');
-			if (closeIcon.classList.contains('active')) {
-				closeIcon.style.zIndex= '5';
-			} else {
-				closeIcon.style.zIndex= '4';
-			}
 		}		
 	}
 
@@ -50,11 +24,9 @@ const Hotbar = () => {
 		<>
 			<div className={styles.navContainer}>
 				<div className={styles.nav}>
-					<div id='menu-icon' className={`${styles.menuContainerMenu}`} onClick={toggleMenu}>
-						<IoMenuSharp className={`${styles.menuIcon} active`} />
-					</div>
-					<div id='close-icon' className={`${styles.menuContainerClose} `} onClick={toggleClose}>
-						<IoCloseCircleOutline className={`${styles.closeIcon} non-active`}/>
+					<div  className={`${styles.menuContainer}`} onClick={toggleMenu}>
+						<IoMenuSharp id='menu-icon' className={`${styles.menuIcon} active`} />
+						<IoCloseCircleOutline id='close-icon' className={`${styles.closeIcon} non-active`}/>
 					</div>
 					<div className={styles.menuBackdrop}></div>					
 					<GoHomeFill className={`${styles.navIcon} ${styles.iconLeft} ms-2 mt-5`} />
