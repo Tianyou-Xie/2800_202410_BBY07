18f32e1a0ae2e3fbc7af3304c878d96224d51033
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-14 17:52:07 -0700

    Signup route add(frontend)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 import { Route, Switch } from 'wouter';
 import IndexPage from './pages';
 import GoodbyePage from './pages/goodbye';
 import About from './pages/about';
+import Signup from './pages/signup';
 
 import './index.css';
 
@@ -14,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 			<Route path='/' component={IndexPage} />
 			<Route path='/goodbye' component={GoodbyePage} />
 			<Route path='/about' component={About} />
+			<Route path='/signup' component={Signup} />
 			<Route>404 Not Found</Route>
 		</Switch>
 	</React.StrictMode>,
