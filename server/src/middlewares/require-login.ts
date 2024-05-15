878ae1e641ad589490baa09a75b3c2f1beb49b59
Zyrakia <ole.lammers@pm.me>
2024-05-15 10:08:14 -0700

    implemented new utilities

--- server/src/middlewares/require-login.ts
+++ server/src/middlewares/require-login.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { Resolve } from '../utils/express';
 
 /**
  * Middleware to ensure that the user object exists in the current session information, if it does exist it means that the given user is currently authenticated.
@@ -6,5 +7,5 @@ import { NextFunction, Request, Response } from 'express';
 export function requireLogin(req: Request, res: Response, next: NextFunction) {
 	if (req.session.user) {
 		next();
-	} else res.status(401).json({ error: 'Authentication is required' });
+	} else Resolve(res).unauthorized('Authentication is required.');
 }
