1f64c23ac958960dca39b81631c3e50af2f4d6fd
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-17 04:31:34 -0700

    signup button redirect added

--- client/src/pages/login/login-html.tsx
+++ client/src/pages/login/login-html.tsx
@@ -1,13 +1,15 @@
+import { useLocation } from 'wouter';
 import styles from './login.module.css';
 import logoUrl from '../../assets/images/SkynetLogo.png';
 
 const LoginHtml = ({ email, password, setEmail, setPassword, submitForm }: any) => {
+	const [_, navigate] = useLocation();
 	return (
 		<div className={styles.loginContainer}>
 			<div className='px-4 text-center'>
 				<img className={`${styles.img} img-fluid`} src={logoUrl} alt='Skynet Logo' />
 				<h1 className={styles.h1}>SKY.NET</h1>
-				<h5 className={styles.h5}>STAY CONNECTED ACROSS THE GALAXY</h5>
+				<h5 className={styles.h5}>Stay Connected Across The Galaxy</h5>
 				<div className={`${styles.loginUpperdiv} mb-1`}></div>
 				<div className={styles.loginForm}>
 					<form onSubmit={submitForm}>
@@ -30,12 +32,16 @@ const LoginHtml = ({ email, password, setEmail, setPassword, submitForm }: any) 
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 						<div className='mb-3'>
-							<button className={`${styles.button}`}>LOGIN</button>
+							<button className={`${styles.button}`}>Login</button>
 						</div>
 					</form>
 					<div className=''>
 						<span className={styles.span}>New User? Signup Below</span>
-						<button className={`${styles.loginButtonLogin} ${styles.button}`}>SIGN UP</button>
+						<button
+							onClick={() => navigate('/signup')}
+							className={`${styles.loginButtonLogin} ${styles.button}`}>
+							Sign Up
+						</button>
 					</div>
 				</div>
 				<div className={`${styles.loginBottomdiv} mt-2`}></div>
