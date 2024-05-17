8b5244a82189f566c429f46871188084a3e8334f
Zyrakia <ole.lammers@pm.me>
2024-05-17 00:14:54 -0700

    check auth before routing

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,5 @@
 import { ToastContainer } from 'react-toastify';
-import { Switch, Route } from 'wouter';
+import { Switch, Route, Redirect, useLocation } from 'wouter';
 import About from './pages/about/about';
 import Changepassword from './pages/changepassword/changepassword';
 import Forgetpassword from './pages/forgetpassword/forgetpassword';
@@ -9,29 +9,39 @@ import Login from './pages/login/login';
 import MyFeed from './pages/my-feed/my-feed';
 import Signup from './pages/signup/signup';
 import Test from './pages/test-page/test-page';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Auth } from './lib/auth';
 
 import './index.css';
 
 export const App = () => {
+	const [authorized, setAuthorized] = useState<boolean | undefined>(undefined);
+
 	useEffect(() => {
 		Auth.resaveToken();
+		Auth.isAuthorized().then((v) => setAuthorized(v));
 	}, []);
 
 	return (
 		<>
 			<ToastContainer />
 			<Switch>
-				<Route path='/' component={Signup} />
-				<Route path='/about' component={About} />
-				<Route path='/signup' component={Signup} />
-				<Route path='/forgetpassword' component={Forgetpassword} />
-				<Route path='/changepassword' component={Changepassword} />
-				<Route path='/login' component={Login} />
-				<Route path='/feed' component={GeneralFeed} />
-				<Route path='/myfeed' component={MyFeed} />
-				<Route path='/home' component={Home} />
+				{authorized === undefined ? (
+					<></>
+				) : (
+					<>
+						<Route path='/about' component={About} />
+						<Route path='/signup' component={Signup} />
+						<Route path='/forgetpassword' component={Forgetpassword} />
+						<Route path='/changepassword' component={Changepassword} />
+						<Route path='/login' component={Login} />
+						<Route path='/feed' component={GeneralFeed} />
+						<Route path='/myfeed' component={MyFeed} />
+						<Route path='/home' component={Home} />
+
+						{authorized === false ? <Redirect href='/login' /> : <Route path='/' component={Home} />}
+					</>
+				)}
 
 				{/* Page to test components */}
 				<Route path='/test' component={Test} />
