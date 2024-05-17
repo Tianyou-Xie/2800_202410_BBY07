59a4df3c74b1d329feff6310cdc7d4bd9005ecb9
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-17 00:20:27 -0700

    fixed missing code from conflict

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,7 +10,7 @@ import About from './pages/about/about';
 import Signup from './pages/signup/signup';
 import Forgetpassword from './pages/forgetpassword/forgetpassword';
 import Changepassword from './pages/changepassword/changepassword';
-import Login from './pages/login/login';
+import Login from './pages/login/login-component';
 import GeneralFeed from './pages/general-feed/general-feed';
 import MyFeed from './pages/my-feed/my-feed';
 import Home from './pages/home/home';
@@ -21,16 +21,18 @@ import { Auth } from './lib/auth';
 Auth.resaveToken();
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	// <React.StrictMode>
-	<Switch>
-		<Route path='/' component={Signup} />
-		<Route path='/about' component={About} />
-		<Route path='/signup' component={Signup} />
-		<Route path='/forgetpassword' component={Forgetpassword} />
-		<Route path='/changepassword' component={Changepassword} />
-		<Route path='/login' component={Login} />
-		<Route path='/feed' component={GeneralFeed} />
-		<Route path='/myfeed' component={MyFeed} />
-		<Route path='/home' component={Home} />
+	<>
+		<ToastContainer />
+		<Switch>
+			<Route path='/' component={Signup} />
+			<Route path='/about' component={About} />
+			<Route path='/signup' component={Signup} />
+			<Route path='/forgetpassword' component={Forgetpassword} />
+			<Route path='/changepassword' component={Changepassword} />
+			<Route path='/login' component={Login} />
+			<Route path='/feed' component={GeneralFeed} />
+			<Route path='/myfeed' component={MyFeed} />
+			<Route path='/home' component={Home} />
 
 			{/* Page to test components */}
 			<Route path='/test' component={Test} />
