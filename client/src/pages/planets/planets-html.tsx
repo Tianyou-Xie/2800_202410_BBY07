118a8de691a0daaaa1d80061b89ffdffb42fe9ef
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-20 07:03:51 -0700

    1. Displaying all the planets in order 2. Positioned sun in the middle 3. Added planets orbits css 4. Animations added for all the planets

--- client/src/pages/planets/planets-html.tsx
+++ client/src/pages/planets/planets-html.tsx
@@ -3,7 +3,34 @@ import styles from './planets.module.css';
 const PlanetsHtml = () => {
 	return (
 		<div className={styles.planetsContainer}>
-			<div className={styles.planetsSun}></div>
+			<div className={styles.sun}></div>
+			<div className={`${styles.orbit} ${styles.mercuryOrbit}`}>
+				<div className={`${styles.planet} ${styles.mercury}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.venusOrbit}`}>
+				<div className={`${styles.planet} ${styles.venus}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.earthOrbit}`}>
+				<div className={`${styles.planet} ${styles.earth}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.marsOrbit}`}>
+				<div className={`${styles.planet} ${styles.mars}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.jupiterOrbit}`}>
+				<div className={`${styles.planet} ${styles.jupiter}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.saturnOrbit}`}>
+				<div className={`${styles.planet} ${styles.saturn}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.uranusOrbit}`}>
+				<div className={`${styles.planet} ${styles.uranus}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.neptuneOrbit}`}>
+				<div className={`${styles.planet} ${styles.neptune}`}></div>
+			</div>
+			<div className={`${styles.orbit} ${styles.plutoOrbit}`}>
+				<div className={`${styles.planet} ${styles.pluto}`}></div>
+			</div>
 		</div>
 	);
 };
