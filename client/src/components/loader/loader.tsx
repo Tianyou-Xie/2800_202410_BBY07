402c775a91d120d3ed15dacac190f75bea128220
Zyrakia <ole.lammers@pm.me>
2024-05-21 19:06:26 -0700

    revised scrambling animation

--- client/src/components/loader/loader.tsx
+++ client/src/components/loader/loader.tsx
@@ -1,9 +1,18 @@
 import styles from './loader.module.css';
+import { Scrambler } from '../scrambler/scrambler';
 
 export const Loader = () => {
+	const text = 'Loading';
+
 	return (
-		<div className={`w-100 h-100 bg-black d-flex flex-column align-items-center justify-content-center`}>
-			<h1 className={`${styles.loadingText} display-4 text-white text-center`}>Loading</h1>
+		<div className={`w-100 h-100 bg-black d-flex flex-column align-items-center justify-content-center z-3`}>
+			<div
+				className={` ${styles.loadingText} d-flex align-items-center justify-content-center position-relative display-3 text-white`}>
+				<span className='opacity-0'>{text}</span>
+				<span className='position-absolute'>
+					<Scrambler text={text} />
+				</span>
+			</div>
 		</div>
 	);
 };
--- client/src/components/loader/small-loader.tsx
+++ client/src/components/loader/small-loader.tsx
@@ -1,34 +1,16 @@
+import { Scrambler } from '../scrambler/scrambler';
 import styles from './small-loader.module.css';
-import { useEffect, useState } from 'react';
 
 export const SmallLoader = () => {
-	const initialText = 'Loading';
-	const characterSet = ['@', '#', '$', '%', '-', '&', '*', '_'];
-
-	const [text, setText] = useState(initialText);
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			const anim = setInterval(() => {
-				const randomChar = initialText.charAt(Math.floor(Math.random() * initialText.length));
-				const randomCharReplacement = characterSet[Math.floor(Math.random() * characterSet.length)];
-				console.log(randomChar, randomCharReplacement);
-				setText(initialText.replace(randomChar, randomCharReplacement));
-			}, 50);
-
-			setTimeout(() => {
-				clearInterval(anim);
-				setText(initialText);
-			}, 750);
-		}, 1000);
-
-		return () => clearInterval(interval);
-	});
+	const text = 'Loading';
 
 	return (
-		<span className={`${styles.loader} text-black d-flex align-items-center justify-content-center position-relative`}>
-			<span className='opacity-0'>{initialText}</span>
-			<span className='position-absolute'>{text}</span>
+		<span
+			className={`${styles.loader} w-100 h-100 text-black d-flex align-items-center justify-content-center position-relative`}>
+			<span className='opacity-0'>{text}</span>
+			<span className='position-absolute'>
+				<Scrambler text={text} />
+			</span>
 		</span>
 	);
 };
