6c4f0f70b9746329a59469e700253c77f67d18b8
MarcusLages <marcusvlages@gmail.com>
2024-05-21 22:02:37 -0700

    Added UIBox component for common textboxes

---
+++ client/src/pages/post-page/post-page.tsx
@@ -0,0 +1,18 @@
+import style from './post-page.module.css';
+import Page from '../../components/Page/Page';
+import ModalConfirmation from '../../components/ModalConfirmation/ModalConfirmation';
+import Post from '../../components/Post/Post';
+
+interface PostProps {}
+
+const PostPage = function (props: PostProps) {
+	let content: JSX.Element = <>{/* <Post/> */}</>;
+
+	return (
+		<>
+			<Page pageName='Post' content={content} />
+		</>
+	);
+};
+
+export default PostPage;
