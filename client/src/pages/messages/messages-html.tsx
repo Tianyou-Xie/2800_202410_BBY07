0027cbc95adf7edd5b09c0a0be3b7c3888a6e0f6
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-23 06:14:11 -0700

    Basic design structure setup for messaging

--- client/src/pages/messages/messages-html.tsx
+++ client/src/pages/messages/messages-html.tsx
@@ -1,36 +1,62 @@
+import { FaVideo } from 'react-icons/fa';
+import { IoCall } from 'react-icons/io5';
+import { IoChevronBackCircleSharp } from 'react-icons/io5';
+import { IoSend } from 'react-icons/io5';
+import { MdOutlineEmojiEmotions } from 'react-icons/md';
+import { RxAvatar } from 'react-icons/rx';
+
 import styles from './messages.module.css';
 
-const MessagesHtml = () => {
+const MessagesHtml = ({ message, setMessage, submitForm }: any) => {
 	return (
 		<>
 			<div className='container'>
 				<header className='border-bottom lh-1 py-3'>
 					<div className='row flex-nowrap justify-content-between align-items-center'>
 						<div className='col-4 pt-1'>
-							<a className='link-secondary' href='#'>
-								Logo
-							</a>
+							<IoChevronBackCircleSharp size={30} />
 						</div>
 						<div className='col-4 text-center'>
 							<a className='blog-header-logo text-body-emphasis text-decoration-none' href='#'>
-								User
+								<RxAvatar size={50} />
+								<h3>Kamal D.</h3>
 							</a>
 						</div>
 						<div className='col-4 d-flex justify-content-end align-items-center'>
-							<a className='link-secondary' href='#' aria-label='Search'></a>
-							<a className='btn btn-sm btn-outline-secondary' href='#'>
-								Sign up
-							</a>
+							<FaVideo size={20} />
+							<IoCall size={20} />
 						</div>
 					</div>
 				</header>
 
-				<div className='nav-scroller py-1 mb-3 border-bottom'>
-					<nav className='nav nav-underline justify-content-between'>
-						<a className='nav-item nav-link link-body-emphasis active' href='#'>
-							World
-						</a>
-					</nav>
+				<div className={`container fixed-bottom mb-2 ${styles.lol}`}>
+					<div className='row'>
+						<div className='col-1 themed-grid-col'>
+							<MdOutlineEmojiEmotions size={40} />
+						</div>
+						<div className='col-10 themed-grid-col'>
+							<form onSubmit={submitForm}>
+								<input
+									type='text'
+									className='form-control form-control-lg'
+									id='address'
+									placeholder='Type Here'
+									required
+									value={message}
+									onChange={(e) => setMessage(e.target.value)}
+								/>
+							</form>
+						</div>
+						<div className='col-1 themed-grid-col'>
+							<IoSend size={40} />
+						</div>
+					</div>
+					{/* <button
+						id='myBtn'
+						type='button'
+						className={`${styles.btnDefault} form-control btn btn-block confirm-button`}>
+						Send
+					</button> */}
 				</div>
 			</div>
 		</>
