b115692f8f8d35b151c77728ae6f7e4337c15999
Zyrakia <ole.lammers@pm.me>
2024-05-22 23:09:24 -0700

    added visual properties to planet model

--- server/src/models/planet.ts
+++ server/src/models/planet.ts
@@ -1,11 +1,28 @@
 import { model, Schema } from 'mongoose';
 
+interface IPlanetVisual {
+	radius: number;
+	orbitRadius: number;
+	orbitDuration?: number;
+}
+
 export interface IPlanet {
 	name: string;
+	visual: IPlanetVisual;
 }
+
+const visualSchema = new Schema<IPlanetVisual>(
+	{
+		radius: { type: 'number', required: true },
+		orbitRadius: { type: 'number', required: true },
+		orbitDuration: { type: 'number' },
+	},
+	{ _id: false },
+);
 
 const schema = new Schema<IPlanet>({
 	name: { type: 'string', required: true },
+	visual: { type: visualSchema, required: true },
 });
 
 export const PlanetModel = model('Planet', schema);
