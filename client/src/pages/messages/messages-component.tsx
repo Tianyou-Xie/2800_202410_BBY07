d37bba20dbcdbe03527b6964c0dde7241d24bfb5
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-23 06:11:18 -0700

    fetching messages onload between two users setup

--- client/src/pages/messages/messages-component.tsx
+++ client/src/pages/messages/messages-component.tsx
@@ -1,7 +1,33 @@
 import MessagesHtml from './messages-html';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
 
 const Messages = () => {
-	return <MessagesHtml />;
+	const [message, setMessage] = useState('');
+
+	useEffect(() => {
+		const fetchMessages = async () => {
+			let headers = {
+				headers: {
+					Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+				},
+			};
+			const { data: res } = await api.post('/user/getchats', { receiverId: '664399aff4853409512e9032' }, headers);
+			try {
+				console.log(res);
+				// setMessage(res.value);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		fetchMessages();
+	}, []);
+
+	const submitForm = async (e: any) => {
+		console.log(message);
+	};
+	return <MessagesHtml email={message} setMessage={setMessage} submitForm={submitForm} />;
 };
 
 export default Messages;
