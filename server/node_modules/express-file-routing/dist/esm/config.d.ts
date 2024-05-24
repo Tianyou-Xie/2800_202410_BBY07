80fbf3482741dee5fe554c4d0aa229a093fbb03f
Zyrakia <ole.lammers@pm.me>
2024-05-24 09:58:25 -0700

    removed all node_modules again

--- server/node_modules/express-file-routing/dist/esm/config.d.ts
+++
@@ -1,7 +0,0 @@
-declare const config: {
-    VALID_FILE_EXTENSIONS: string[];
-    INVALID_NAME_SUFFIXES: string[];
-    IGNORE_PREFIX_CHAR: string;
-    DEFAULT_METHOD_EXPORTS: string[];
-};
-export default config;
