4481d382288c15b86d0051afb2431adb0c6a3963
MarcusLages <marcusvlages@gmail.com>
2024-05-24 00:18:00 -0700

    Modified the general page to redirect to planet pages (HARD CODED)
    (TODO) Automate the process of listing the planets. Waiting for the true finished planet home page, so not a priority

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -5,12 +5,12 @@ import { Link } from 'wouter';
 
 interface PlanetProps {
 	planet: string;
-	url: string;
+	planetId: string;
 }
 
 const Planets = (props: PlanetProps) => {
 	return (
-		<Link href={props.url} className={styles.planetsLink}>
+		<Link href={`/planet/${props.planet}/${props.planetId}`} className={styles.planetsLink}>
 			<div className={styles.planets}>{props.planet}</div>
 		</Link>
 	);
@@ -21,16 +21,16 @@ const Home = () => {
 		<div className='d-flex align-items-center justify-content-center p-3'>
 			<div className={styles.mainContainer}>
 				<h1 className={styles.title}>Choose your planet</h1>
-				<Planets planet='GENERAL FEED' url='/feed' />
-				<Planets planet='Earth' url='/feed' />
-				<Planets planet='Moon' url='/feed' />
-				<Planets planet='Mercury' url='/feed' />
-				<Planets planet='Venus' url='/feed' />
-				<Planets planet='Mars' url='/feed' />
-				<Planets planet='Jupiter' url='/feed' />
-				<Planets planet='Saturn' url='/feed' />
-				<Planets planet='Uranus' url='/feed' />
-				<Planets planet='Neptune' url='/feed' />
+				<Planets planet='GENERAL FEED' planetId='/feed' />
+				<Planets planet='Earth' planetId='664399a3036de6e77d00332f' />
+				<Planets planet='Mercury' planetId='66439add036de6e77d003330' />
+				<Planets planet='Venus' planetId='66439af3036de6e77d003331' />
+				<Planets planet='Mars' planetId='66439b04036de6e77d003332' />
+				<Planets planet='Jupiter' planetId='66439b0e036de6e77d003333' />
+				<Planets planet='Saturn' planetId='66439b34036de6e77d003334' />
+				<Planets planet='Uranus' planetId='66439b3a036de6e77d003335' />
+				<Planets planet='Neptune' planetId='66439b41036de6e77d003336' />
+				<Planets planet='Xenos-Prime' planetId='665005bbaac7a8f6c7fa1d1c' />
 			</div>
 		</div>
 	);
