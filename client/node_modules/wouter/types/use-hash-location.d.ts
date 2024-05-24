26e24c4aef6f34c169929658037ed52ada5a770d
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-05-24 09:40:23 -0700

    fixed local gitignore

--- client/node_modules/wouter/types/use-hash-location.d.ts
+++
@@ -1,7 +0,0 @@
-import { Path } from "./location-hook.js";
-
-export function navigate<S = any>(to: Path, options?: { state: S }): void;
-
-export function useHashLocation(options?: {
-  ssrPath?: Path;
-}): [Path, typeof navigate];
