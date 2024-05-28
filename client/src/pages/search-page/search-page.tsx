547beee879eddcfd7e781881550dfcae1a753c8b
MarcusLages <marcusvlages@gmail.com>
2024-05-28 12:10:50 -0700

    Updated the search route for better pagination

--- client/src/pages/search-page/search-page.tsx
+++ client/src/pages/search-page/search-page.tsx
@@ -1,11 +1,61 @@
 import styles from './search-page.module.css';
 import { useEffect, useState } from 'react';
 import { api } from '../../lib/axios';
-import { UserAuthContext } from '../../lib/auth';
 import Page from '../../components/Page/Page';
+import UIBox from '../../components/UIBox/UIBox';
+import { Container } from 'react-bootstrap';
+import { PaginatedPostFeed } from '../../components/paginated-post-feed/paginated-post-feed';
 
 const SearchPage = function () {
-	return (<Page content={<p>page</p>}/>)
+	const [searchKey, setSearchKey] = useState('');
+	const [feedComponent, setFeedComponent] = useState(<></>);
+
+	useEffect(() => {
+		// let feed = (
+		// 	<PaginatedPostFeed
+		// 		feedKey={'search'}
+		// 		fetchPage={(page) => api.get(`/feed/${searchKey}?page=${page}`).then((res) => res.data.value.posts)}
+		// 	/>
+		// );
+		setFeedComponent(<>{searchKey}</>);
+	}, [searchKey]);
+
+	function doSearch() {
+		const search = document.getElementsByTagName('input');
+		const content = search[0].value;
+		setSearchKey(content);
+	}
+
+	return (
+		<Page
+			content={
+				<>
+					<Container className={styles.general}>
+						<UIBox
+							className={styles.search}
+							content={
+								<input
+									type='text'
+									id='search-bar'
+									name='search'
+									className={styles.searchBox}
+									placeholder='Search for anything in the universe...'
+									autoComplete='off'
+									onChange={doSearch}></input>
+							}
+							curved
+						/>
+						<div className={styles.sideButtons}>
+							<button className={styles.submit} onClick={doSearch}>
+								<UIBox className={styles.submitBox} content='Search' curved dark clickable />
+							</button>
+						</div>
+					</Container>
+					<Container>{feedComponent}</Container>
+				</>
+			}
+		/>
+	);
 };
 
 export default SearchPage;
--- server/src/routes/search/[search].ts
+++ server/src/routes/search/[search].ts
@@ -1,8 +1,9 @@
 import { Handler } from 'express';
 import { Resolve } from '../../utils/express';
-import { UserModel } from '../../models/user';
-import { PostModel } from '../../models/post';
+import { IUser, UserModel } from '../../models/user';
+import { IPost, PostModel } from '../../models/post';
 import { authProtected } from '../../middlewares/auth-protected';
+import mongoose from 'mongoose';
 
 /**
  * GET @ /search/:search
@@ -14,18 +15,63 @@ export const get: Handler[] = [
 	authProtected,
 	async (req, res) => {
 		const search = req.params.search;
-		const users = await UserModel.find({ userName: { $regex: search, $options: 'si' } })
-			.lean()
-			.select('userName bio location avatarUrl followerCount followingCount postCount createdAt');
-		const posts = await PostModel.find({ content: { $regex: search, $options: 'si' } })
-			.lean()
-			.select('authorId content likeCount commentCount location deleted isRoot media createdAt');
-		if ((!users || users.length == 0) && (!posts || posts.length == 0)) {
-			Resolve(res).notFound('No user or posts found by the given username.');
-		} else
-			Resolve(res).okWith({
-				users: users,
-				posts: posts,
-			});
+
+		let userSearch: mongoose.FilterQuery<IUser>;
+		let postSearch: mongoose.FilterQuery<IPost>;
+		let searchedPosts: any[] = [];
+		let searchedUsers: any[] = [];
+
+		const rawPage = parseInt(typeof req.query.page === 'string' ? req.query.page : '');
+		const page = Math.max(1, typeof rawPage !== 'number' || isNaN(rawPage) ? 1 : rawPage);
+		const limit = 20;
+		const skip = (page - 1) * limit;
+
+		const users = await UserModel.exists({ userName: { $regex: search, $options: 'si' } }).lean();
+		const posts = await PostModel.exists({ content: { $regex: search, $options: 'si' } }).lean();
+
+		if (!users && !posts) return Resolve(res).notFound('No user or posts found with this search key.');
+		if (users) {
+			userSearch = { userName: { $regex: search, $options: 'si' } };
+			searchedUsers = await UserModel.aggregate([
+				{ $match: { ...userSearch } },
+				{ $sort: { createdAt: -1 } },
+				{ $skip: skip },
+				{ $limit: limit },
+				{
+					$project: {
+						userName: 1,
+						avatarUrl: 1,
+						followerCount: 1,
+						followingCount: 1,
+						postCount: 1,
+					},
+				},
+			]);
+		}
+		if (posts) {
+			postSearch = { content: { $regex: search, $options: 'si' } };
+			searchedPosts = await PostModel.aggregate([
+				{ $match: { ...postSearch, deleted: { $not: { $eq: true } } } },
+				{ $sort: { likeCount: -1, commentCount: -1, createdAt: -1 } },
+				{ $skip: skip },
+				{ $limit: limit },
+				{
+					$lookup: {
+						from: 'users',
+						localField: 'authorId',
+						foreignField: '_id',
+						as: 'author',
+					},
+				},
+				{ $unwind: { path: '$author' } },
+				{ $addFields: { userName: '$author.userName' } },
+				{ $project: { author: 0 } },
+			]);
+		}
+
+		Resolve(res).okWith({
+			users: searchedUsers,
+			posts: searchedPosts,
+		});
 	},
 ];
