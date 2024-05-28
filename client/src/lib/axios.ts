fd688bb8d3a3cbe1288a5e144082fa7bcd76cc50
Zyrakia <ole.lammers@pm.me>
2024-05-28 11:25:14 -0700

    added caching with axios-cache-interceptor

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,26 +1,40 @@
-import axios, { AxiosError, HttpStatusCode } from 'axios';
+import axios from 'axios';
 import { getServerHost } from '../environment';
+import { buildWebStorage, setupCache } from 'axios-cache-interceptor';
+
+/**
+ * The server hostname, used to prefix
+ * all API requests.
+ */
+const host = getServerHost();
+
+/**
+ * The path patterns that will be cached by the API cache.
+ * Cache TTL is 5 minutes by default.
+ */
+let cachedPaths: string[] = ['^/planet.*$', '^/post/.*/(like|save|parent)$'];
+
+// TODO invalidate when saving or liking
 
 /**
  * API Axios instance to access server API routes.
  *
- * This automatically handles the authorization header
- * and URL host.
+ * This automatically handles the authorization header,
+ * URL host prefixing and caching.
  */
-export const api = axios.create({
-	baseURL: getServerHost(),
-});
+export const api = setupCache(axios.create({ baseURL: host }), {
+	storage: buildWebStorage(localStorage, 'skynet-cache:'),
+	cachePredicate: {
+		responseMatch: (res) => {
+			const req = res.request as XMLHttpRequest;
+			if (!req) return false;
 
-api.interceptors.response.use(
-	(res) => {
-		if (res.status === HttpStatusCode.Unauthorized) location.assign('/login');
-		return res;
+			const path = req.responseURL.replace(host, '');
+			for (const pattern of cachedPaths) {
+				if (path.match(pattern)) return true;
+			}
+
+			return false;
+		},
 	},
-	(err) => {
-		if (!(err instanceof AxiosError)) return Promise.reject(err);
-		if (err.response?.status === HttpStatusCode.Unauthorized && location.pathname !== '/login')
-			// 	location.assign('/login');
-			console.log(err);
-		return Promise.reject(err);
-	},
-);
+});
