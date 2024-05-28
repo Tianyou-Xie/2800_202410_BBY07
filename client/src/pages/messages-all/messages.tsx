efd9fa203e5c62302ba2d5f517c2fdf651dab13e
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-28 12:49:06 -0700

    fetching user ids from api user/conversations

--- client/src/pages/messages-all/messages.tsx
+++ client/src/pages/messages-all/messages.tsx
@@ -1,35 +1,44 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
 import Page from '../../components/Page/Page';
 import styles from './messages.module.css';
 
 const MessagesAll = () => {
+	const [users, setUsers] = useState([]);
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const { data: res } = await api.post('/user/conversations');
+			try {
+				if (res.success) {
+					console.log(res.data);
+					setUsers(res.data);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		fetchUsers();
+	}, []);
 	return (
 		<Page
 			pageName='Messages'
 			content={
 				<div className='container mt-5'>
 					<ul className='list-group'>
-						<li className='list-group-item d-flex justify-content-between align-items-center'>
-							<div className='d-flex align-items-center'>
-								<div className={`${styles.messageIcon} mr-3`}>
-									<span>A</span>
+						{users.map((user, index) => (
+							<li key={index} className='list-group-item d-flex justify-content-between align-items-center'>
+								<div className='d-flex align-items-center'>
+									<div className={`${styles.messageIcon} mr-3`}>
+										<span>A</span>
+									</div>
+									<div>
+										<h5 className='mb-0'>Kamal Dolikay</h5>
+									</div>
 								</div>
-								<div>
-									<h5 className='mb-0'>Alice Johnson</h5>
-								</div>
-							</div>
-							<small className='text-muted'>2024-05-21</small>
-						</li>
-						<li className='list-group-item d-flex justify-content-between align-items-center'>
-							<div className='d-flex align-items-center'>
-								<div className={`${styles.messageIcon} mr-3`}>
-									<span>B</span>
-								</div>
-								<div>
-									<h5 className='mb-0'>Bob Smith</h5>
-								</div>
-							</div>
-							<small className='text-muted'>2024-05-20</small>
-						</li>
+								<small className='text-muted'>2024-05-21</small>
+							</li>
+						))}
 					</ul>
 				</div>
 			}
