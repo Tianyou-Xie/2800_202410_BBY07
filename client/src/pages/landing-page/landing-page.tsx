73a085d0ad29e77c523161bb67ad2faee3925798
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-05-28 14:00:32 -0700

    started a basic layout to the page

--- client/src/pages/landing-page/landing-page.tsx
+++ client/src/pages/landing-page/landing-page.tsx
@@ -3,31 +3,40 @@ import styles from './landing-page.module.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { PlanetMap } from '../planet-map/planet-map';
+import { Scrambler } from '../../components/scrambler/scrambler'
+import logoUrl from '../../assets/images/SkynetLogo.png';;
 
 const LandingPage = () => {
 	return (
 		<>
-			<div className={styles.nav}>
-				<Navbar expand='lg' className='bg-body-tertiary'>
-					<Container className='w-100'>
-						<Navbar.Brand href='/'>sky.net</Navbar.Brand>
-						<div>
-							<button
-								className={`${styles.landingBtn} ${styles.loginBtn} btn btn-primary me-2`}
-								type='submit'>
-								Login
-							</button>
-							<button
-								className={`${styles.landingBtn} ${styles.signupBtn} btn btn-primary ms-2`}
-								type='button'>
-								Signup
-							</button>
-						</div>
-					</Container>
-				</Navbar>
-			</div>
-			<div className={styles.planetMap}>
-				<PlanetMap interactable={false} />
+			<div className={styles.landingDisplay}>
+				<div className={styles.nav}>
+					<Navbar expand='lg' className='bg-body-tertiary'>
+						<Container className={`${styles.navContainer} w-100`}>
+							<Navbar.Brand href='/'>
+							<img className={styles.logo} src={logoUrl} alt='Skynet Logo' />
+								<Scrambler text='sky.net' scrambleSpeed={80} scrambleDelay={6000} />
+							</Navbar.Brand>
+							<div>
+								<button className={`${styles.landingBtn} ${styles.loginBtn} btn me-2`} type='submit'>
+									Login
+								</button>
+								<button className={`${styles.landingBtn} ${styles.signupBtn} btn ms-2`} type='button'>
+									Signup
+								</button>
+							</div>
+						</Container>
+					</Navbar>
+				</div>
+				<div className={`${styles.planetMap}`}>
+					<div className={styles.backdrop}></div>
+					<PlanetMap interactable={false} />
+				</div>
+				<div className={`${styles.landingHeader} w-100 text-center`}>
+					<h1><Scrambler text='Communication Revisioned' scrambleSpeed={80} scrambleDelay={6000} /></h1>
+					<h3>Welcome to sky.net</h3>
+				</div>
+				<div>sadasd</div>
 			</div>
 		</>
 	);
