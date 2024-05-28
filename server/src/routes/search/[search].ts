f7ae3017d8d2c67c5a49a36e268c442c91572b00
MarcusLages <marcusvlages@gmail.com>
2024-05-27 21:12:31 -0700

    Modified route to search for posts and users

---
+++ server/src/routes/search/[search].ts
@@ -0,0 +1,28 @@
+import { Handler } from 'express';
+import { Resolve } from '../../utils/express';
+import { UserModel } from '../../models/user';
+import { PostModel } from '../../models/post';
+import { options } from 'joi';
+
+/**
+ * GET @ /search/:search
+ *
+ * This searches in the database for a user or a post with or that contains the specified string
+ * passed as an url parameter.
+ */
+export const get: Handler = async (req, res) => {
+	const search = req.params.search;
+	const users = await UserModel.find({ userName: { $regex: search, $options: 'si' } })
+		.lean()
+		.select('userName bio location avatarUrl followerCount followingCount postCount createdAt');
+	const posts = await PostModel.find({ content: { $regex: search, $options: 'si' } })
+		.lean()
+		.select('authorId content likeCount commentCount location deleted isRoot media createdAt');
+	if (!users && !posts) {
+		Resolve(res).notFound('No user or posts found by the given username.');
+	} else
+		Resolve(res).okWith({
+			users: users,
+			posts: posts,
+		});
+};
