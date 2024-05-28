8dd2e97e4a13ad904bbfad2157f56cdd49a07ab9
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-28 14:56:07 -0700

    Added hover and few styling effects

--- client/src/pages/messages-all/messages.tsx
+++ client/src/pages/messages-all/messages.tsx
@@ -29,7 +29,7 @@ const MessagesAll = () => {
 					<ul className='list-group'>
 						{users.map((user: any, index) => (
                             <Link key={index} href={`/messages/${user.userId}`} className={styles.userLink}>
-							<li key={index} className='list-group-item d-flex justify-content-between align-items-center'>
+							<li key={index} className={`${styles.listGroupItem} list-group-item d-flex justify-content-between align-items-center`}>
 								<div className='d-flex align-items-center'>
                                     <img
                                         src={user.avatar}
