b82c6b01d1716b0c58f1cec6a9b9c4bac51120aa
Kamal Dolikay <kamaldolikay@gmail.com>
2024-05-28 00:52:33 -0700

    Adjusted spacing, message time shown, users avatars shown, made it pretty

--- client/src/pages/messages/messages-html.tsx
+++ client/src/pages/messages/messages-html.tsx
@@ -1,6 +1,6 @@
 import { FaVideo } from 'react-icons/fa';
 import { IoCall } from 'react-icons/io5';
-import { IoChevronBackCircleSharp } from 'react-icons/io5';
+import { IoArrowBackCircle } from "react-icons/io5";
 import { IoSend } from 'react-icons/io5';
 import { MdOutlineEmojiEmotions } from 'react-icons/md';
 import { RxAvatar } from 'react-icons/rx';
@@ -8,7 +8,7 @@ import { Else, If, Then } from 'react-if';
 
 import styles from './messages.module.css';
 
-const MessagesHtml = ({ message, messages, setMessage, submitForm, id, isChat, username }: any) => {
+const MessagesHtml = ({ message, messages, setMessage, submitForm, id, isChat, username, avatar, userImage }: any) => {
 	function handlePageReturn() {
 		history.back();
 	}
@@ -18,22 +18,22 @@ const MessagesHtml = ({ message, messages, setMessage, submitForm, id, isChat, u
 				<header className='border-bottom lh-1 py-3'>
 					<div className='row flex-nowrap justify-content-between align-items-center'>
 						<div className='col-4 pt-1'>
-							<IoChevronBackCircleSharp size={30} onClick={handlePageReturn} />
+							<IoArrowBackCircle style={{color: "#2196F3"}} size={40} onClick={handlePageReturn} />
 						</div>
 						<div className='col-4 text-center'>
 							<a className='blog-header-logo text-body-emphasis text-decoration-none' href='#'>
-								<RxAvatar size={50} />
+								<img src={avatar} width='80' className='rounded-circle' />
 								<h3 className={styles.h3}>{username}</h3>
 							</a>
 						</div>
 						<div className='col-4 d-flex justify-content-end align-items-center'>
-							<FaVideo size={20} />
-							<IoCall size={20} />
+							{/* <FaVideo size={20} />
+							<IoCall size={20} /> */}
 						</div>
 					</div>
 				</header>
 
-				<main className='container'>
+				<main className='container mt-4'>
 					<div className='row'>
 						{isChat ? (
 							messages.map((message: any, index: number) => {
@@ -41,21 +41,31 @@ const MessagesHtml = ({ message, messages, setMessage, submitForm, id, isChat, u
 									<div className='d-inline' key={index}>
 										<If condition={id === message.senderId}>
 											<Then>
-												<div className='d-flex text-body-secondary'>
-													<RxAvatar size={25} />
-													<p className={styles.p} key={index}>
+												<div className={`text-body-secondary `}>
+													<img
+														src={avatar}
+														width='30'
+														height='30'
+														className='rounded-circle'
+													/>
+													<p className={`${styles.p} d-inline mx-2`} key={index}>
 														{message.content}
 													</p>
-													<br />
+                                                    <p className={`${styles.time} ms-4`}>{new Date(message.createdAt).toLocaleTimeString('en-US')}</p>
 												</div>
 											</Then>
 											<Else>
-												<div className='d-flex justify-content-end '>
-													<RxAvatar size={25} />
-													<p className={styles.p} key={index}>
+												<div className='text-end justify-content-end'>
+													<img
+														src={userImage}
+														width='30'
+														height='30'
+														className='rounded-circle'
+													/>
+													<p className={`${styles.p} d-inline mx-2`} key={index}>
 														{message.content}
 													</p>
-													<br />
+													<p className={`${styles.time} me-3`}>{new Date(message.createdAt).toLocaleTimeString('en-US')}</p>
 												</div>
 											</Else>
 										</If>
@@ -70,34 +80,28 @@ const MessagesHtml = ({ message, messages, setMessage, submitForm, id, isChat, u
 					</div>
 				</main>
 
-				<div className={`container fixed-bottom mb-2 ${styles.lol}`}>
-					<div className='row'>
-						<div className='col-1 themed-grid-col'>
-							<MdOutlineEmojiEmotions size={40} />
+				<div className={`container fixed-bottom mb-2`}>
+					<div className='row align-items-center'>
+						<div className={`col-1 themed-grid-col ${styles.customCol}`}>
+							<MdOutlineEmojiEmotions style={{color: "#2196F3"}} size={40} />
 						</div>
-						<div className='col-10 themed-grid-col'>
+						<div className={`col-10 themed-grid-col ${styles.customCol10}`}>
 							<form onSubmit={submitForm}>
 								<input
 									type='text'
-									className='form-control form-control-lg'
+									className={`form-control form-control-lg ${styles.input}`}
 									id='address'
-									placeholder='Type Here'
+									placeholder='type here'
 									required
 									value={message}
 									onChange={(e) => setMessage(e.target.value)}
 								/>
 							</form>
 						</div>
-						<div className='col-1 themed-grid-col'>
-							<IoSend size={40} />
+						<div className={`col-1 themed-grid-col ${styles.customCol}`}>
+							<IoSend style={{color: "#2196F3"}} size={40} onClick={submitForm} />
 						</div>
 					</div>
-					{/* <button
-						id='myBtn'
-						type='button'
-						className={`${styles.btnDefault} form-control btn btn-block confirm-button`}>
-						Send
-					</button> */}
 				</div>
 			</div>
 		</>
