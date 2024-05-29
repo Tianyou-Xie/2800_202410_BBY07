edf004d9de3115dad22c2199f239e0d744de0090
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-05-29 16:20:43 -0700

    added change bio api route

---
+++ server/src/routes/user/changeBio.ts
@@ -0,0 +1,39 @@
+import Joi from 'joi';
+import { Handler } from 'express';
+import { Resolve } from '../../utils/express';
+import { authProtected } from '../../middlewares/auth-protected';
+
+interface bioBody {
+	newBio: string;
+}
+
+/**
+ * PATCH @ /user/changeBio
+ *
+ * This changes the bio for the user
+ * making the request.
+ */
+export const patch: Handler[] = [
+	authProtected,
+	async (req, res) => {
+		const user = req.user!;
+
+		const currBio = user.bio;
+
+		const emailSchema = Joi.object<bioBody>({
+			newBio: Joi.string().trim().required().invalid(currBio).messages({
+				'string.base': 'New bio must be a string.',
+				'any.required': 'A bio is required in order to change your bio.',
+				'any.invalid': 'Bio entered is already your current bio.',
+			}),
+		});
+
+		const validationResult = emailSchema.validate(req.body);
+		if (validationResult.error) return Resolve(res).badRequest(validationResult.error.message);
+
+		const value = validationResult.value;
+
+		const result = await user.updateOne({ bio: value.newBio });
+		return Resolve(res).okWith(result, 'Bio changed successfully.');
+	},
+];
