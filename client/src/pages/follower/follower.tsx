cdf1ee4376f8d724a9feda010189157e13a51b2e
Zyrakia <ole.lammers@pm.me>
2024-05-30 09:40:02 -0700

    fixed width of follower and following user boxes

--- client/src/pages/follower/follower.tsx
+++ client/src/pages/follower/follower.tsx
@@ -1,61 +1,62 @@
-import { useEffect, useState } from "react";
-import { Link } from "wouter";  // Import Link from wouter
-import { api } from "../../lib/axios";
-import Page from "../../components/Page/Page";
-import styles from './follower.module.css';  // Import the CSS module
+import { useEffect, useState } from 'react';
+import { Link } from 'wouter'; // Import Link from wouter
+import { api } from '../../lib/axios';
+import Page from '../../components/Page/Page';
+import styles from './follower.module.css'; // Import the CSS module
 
 interface PostResponse {
-    statusCode: number;
-    statusMessage: string;
-    value: UserProp[];
-    success: boolean;
+	statusCode: number;
+	statusMessage: string;
+	value: UserProp[];
+	success: boolean;
 }
 
 interface UserProp {
-    _id: string;
-    userName: string;
-    followerCount: number;
-    followingCount: number;
-    postCount: number;
-    avatarUrl?: string;
+	_id: string;
+	userName: string;
+	followerCount: number;
+	followingCount: number;
+	postCount: number;
+	avatarUrl?: string;
 }
 
 const FollowerPage = () => {
-    const [followerUsers, setFollowerUsers] = useState<UserProp[]>([]);
+	const [followerUsers, setFollowerUsers] = useState<UserProp[]>([]);
 
-    useEffect(() => {
-        const fetchFollower = async () => {
-            try {
-                const response = await api.get<PostResponse>(`/user/follower`);
-                if (response.data.success) setFollowerUsers(response.data.value);
-            } catch { }
-        };
+	useEffect(() => {
+		const fetchFollower = async () => {
+			try {
+				const response = await api.get<PostResponse>(`/user/follower`);
+				if (response.data.success) setFollowerUsers(response.data.value);
+			} catch {}
+		};
 
-        fetchFollower();
-    }, []);
+		fetchFollower();
+	}, []);
 
-    return (
-        <Page
-            pageName='Follower'
-            content={
-                <div className={styles.userList}>
-                    {followerUsers.map((user) => (
-                        <Link key={user._id} href={`/user/${user._id}`} className={styles.userLink}>
-                            <div className={styles.userItem}>
-                                <img src={user.avatarUrl} alt={user.userName} className={styles.avatar} />
-                                <div className={styles.userInfo}>
-                                    <p className={styles.userName}>{user.userName}</p>
-                                    <p>Followers: {user.followerCount}</p>
-                                    <p>Following: {user.followingCount}</p>
-                                    <p>Posts: {user.postCount}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            }
-        />
-    );
+	return (
+		<Page
+			pageName='Follower'
+			content={
+				<div className={`${styles.userList} container`}>
+					{followerUsers.map((user) => (
+						<Link key={user._id} href={`/user/${user._id}`} className={styles.userLink}>
+							<div className={styles.userItem}>
+								<img src={user.avatarUrl} alt={user.userName} className={styles.avatar} />
+								<div
+									className={`${styles.userInfo} d-flex flex-column p-1 flex-sm-row gap-sm-3 align-items-sm-center`}>
+									<p className={styles.userName}>{user.userName}</p>
+									<p>Followers: {user.followerCount}</p>
+									<p>Following: {user.followingCount}</p>
+									<p>Posts: {user.postCount}</p>
+								</div>
+							</div>
+						</Link>
+					))}
+				</div>
+			}
+		/>
+	);
 };
 
 export default FollowerPage;
--- client/src/pages/following/following.tsx
+++ client/src/pages/following/following.tsx
@@ -1,61 +1,62 @@
-import { useEffect, useState } from "react";
-import { Link } from "wouter";  // Import Link from wouter
-import { api } from "../../lib/axios";
-import Page from "../../components/Page/Page";
-import styles from './following.module.css';  // Import the CSS module
+import { useEffect, useState } from 'react';
+import { Link } from 'wouter'; // Import Link from wouter
+import { api } from '../../lib/axios';
+import Page from '../../components/Page/Page';
+import styles from './following.module.css'; // Import the CSS module
 
 interface PostResponse {
-    statusCode: number;
-    statusMessage: string;
-    value: UserProp[];
-    success: boolean;
+	statusCode: number;
+	statusMessage: string;
+	value: UserProp[];
+	success: boolean;
 }
 
 interface UserProp {
-    _id: string;
-    userName: string;
-    followerCount: number;
-    followingCount: number;
-    postCount: number;
-    avatarUrl?: string;
+	_id: string;
+	userName: string;
+	followerCount: number;
+	followingCount: number;
+	postCount: number;
+	avatarUrl?: string;
 }
 
 const FollowingPage = () => {
-    const [followingUsers, setFollowingUsers] = useState<UserProp[]>([]);
+	const [followingUsers, setFollowingUsers] = useState<UserProp[]>([]);
 
-    useEffect(() => {
-        const fetchFollowing = async () => {
-            try {
-                const response = await api.get<PostResponse>(`/user/following`);
-                if (response.data.success) setFollowingUsers(response.data.value);
-            } catch { }
-        };
+	useEffect(() => {
+		const fetchFollowing = async () => {
+			try {
+				const response = await api.get<PostResponse>(`/user/following`);
+				if (response.data.success) setFollowingUsers(response.data.value);
+			} catch {}
+		};
 
-        fetchFollowing();
-    }, []);
+		fetchFollowing();
+	}, []);
 
-    return (
-        <Page
-            pageName='Following'
-            content={
-                <div className={styles.userList}>
-                    {followingUsers.map((user) => (
-                        <Link key={user._id} href={`/user/${user._id}`} className={styles.userLink}>
-                            <div className={styles.userItem}>
-                                <img src={user.avatarUrl} alt={user.userName} className={styles.avatar} />
-                                <div className={styles.userInfo}>
-                                    <p className={styles.userName}>{user.userName}</p>
-                                    <p>Followers: {user.followerCount}</p>
-                                    <p>Following: {user.followingCount}</p>
-                                    <p>Posts: {user.postCount}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            }
-        />
-    );
+	return (
+		<Page
+			pageName='Following'
+			content={
+				<div className={`${styles.userList} container`}>
+					{followingUsers.map((user) => (
+						<Link key={user._id} href={`/user/${user._id}`} className={styles.userLink}>
+							<div className={styles.userItem}>
+								<img src={user.avatarUrl} alt={user.userName} className={styles.avatar} />
+								<div
+									className={`${styles.userInfo} d-flex flex-column p-1 flex-sm-row gap-sm-3 align-items-sm-center`}>
+									<span className={styles.userName}>{user.userName}</span>
+									<span>Followers: {user.followerCount}</span>
+									<span>Following: {user.followingCount}</span>
+									<span>Posts: {user.postCount}</span>
+								</div>
+							</div>
+						</Link>
+					))}
+				</div>
+			}
+		/>
+	);
 };
 
 export default FollowingPage;
